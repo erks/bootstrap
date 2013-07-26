76ef852e5c3d957148a8eabf0c3add68cdec7a2d
Touch Ungboriboonpisal <touch@ungboriboonpisal.com>
2013-07-26 00:45:22 -0700

    add xcode aliases

--- env/profile.sh
+++ env/profile.sh
@@ -43,3 +43,8 @@ fi
 
 source $env_path/brew.sh
 
+# xcode
+alias symbolicatecrash='/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/PrivateFrameworks/DTDeviceKit.framework/Versions/A/Resources/symbolicatecrash'
+export DEVELOPER_DIR='/Applications/Xcode.app/Contents/Developer'
+
+
