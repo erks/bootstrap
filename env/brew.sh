2570922ee0ff3187d1a00d326aa82a76cbd7531e
Touch Ungboriboonpisal <touch@ungboriboonpisal.com>
2016-09-04 14:23:57 -0700

    remove bad symlink

--- env/brew.sh
+++ env/brew.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-link "/usr/local/Library/Contributions/brew_bash_completion.sh" "/usr/local/etc/bash_completion.d"
 if [ -f `brew --prefix`/etc/bash_completion ]; then
   . `brew --prefix`/etc/bash_completion
 fi
