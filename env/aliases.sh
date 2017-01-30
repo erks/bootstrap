54ad6094bc37cb5c396936cb3d061dbf6f600b3c
Touch Ungboriboonpisal <touch@ungboriboonpisal.com>
2017-01-30 14:41:49 -0800

    move from dropbox to gdrive

--- env/aliases.sh
+++ env/aliases.sh
@@ -6,7 +6,3 @@ alias vim="vim -X"
 alias ls="ls -GFh"
 alias grep="grep --color=auto"
 alias mkdir="mkdir -p"
-
-# xcode
-alias symbolicatecrash='/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/PrivateFrameworks/DTDeviceKit.framework/Versions/A/Resources/symbolicatecrash -v'
-alias symbolicate='atos -arch armv7 -o'
