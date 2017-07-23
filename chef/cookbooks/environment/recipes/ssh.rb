9636b7d1236485bcce21d9f3e19710da8e5b5cbc
Touch Ungboriboonpisal <erks@users.noreply.github.com>
2017-07-23 11:11:19 -0700

    Update ssh.rb

--- chef/cookbooks/environment/recipes/ssh.rb
+++ chef/cookbooks/environment/recipes/ssh.rb
@@ -1,5 +1,5 @@
 home = ENV['HOME']
-key_path = "#{home}/Google Drive/keys/touch@ungboriboonpisal.com.nopass"
+key_path = "#{home}/Google Drive/keys/touch@ungboriboonpisal.com"
 
 directory "#{home}/.ssh" do
     recursive true
