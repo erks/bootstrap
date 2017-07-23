552c334966199072445033861a25e04576ec9249
Touch Ungboriboonpisal <erks@users.noreply.github.com>
2017-07-23 10:20:02 -0700

    Update ssh.rb

--- chef/cookbooks/environment/recipes/ssh.rb
+++ chef/cookbooks/environment/recipes/ssh.rb
@@ -1,18 +1,18 @@
 home = ENV['HOME']
+key_path = "#{home}/Google Drive/keys/touch@ungboriboonpisal.com.nopass"
 
 directory "#{home}/.ssh" do
     recursive true
     owner ENV["USER"]
 end
 
-cookbook_file "#{home}/.ssh/config" do
+template "#{home}/.ssh/config" do
     source "ssh_config"
     mode "600"
     owner ENV["USER"]
+    variables key_path: key_path
     action :create_if_missing
 end
-
-key_path = "#{home}/Google Drive/keys/touch@ungboriboonpisal.com.nopass"
 
 file key_path do
     mode "600"
