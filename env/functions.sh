781f30a0dbcdcbd2ee77f691eb2f891b4c18989f
Touch Ungboriboonpisal <touch@ungboriboonpisal.com>
2017-12-10 14:57:49 -0800

    Revert "switch to icloud drive"
    This reverts commit 370d49c286c12ec4ffee8f9c6e46bd8d50918c13.

--- env/functions.sh
+++ env/functions.sh
@@ -10,6 +10,6 @@ function update() {
 
 function sshkey() {
     echo "updating ssh public key..."
-    scp "$HOME/Library/Mobile Documents/com~apple~CloudDocs/keys/touch@ungboriboonpisal.com.pub" $1:.ssh/id_rsa.pub
+    scp "$HOME/Google Drive/keys/touch@ungboriboonpisal.com.pub" $1:.ssh/id_rsa.pub
     ssh $1 "cat .ssh/id_rsa.pub >> .ssh/authorized_keys; chmod 755 $HOME; chmod 755 $HOME/.ssh; chmod 644 $HOME/.ssh/authorized_keys"
 }
