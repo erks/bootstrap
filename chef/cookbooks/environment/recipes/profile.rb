84d97df4426d1ebe2668033de7b448c06528587b
Touch Ungboriboonpisal <touch@ungboriboonpisal.com>
2020-10-23 22:37:45 -0700

    clean up

--- chef/cookbooks/environment/recipes/profile.rb
+++ chef/cookbooks/environment/recipes/profile.rb
@@ -5,12 +5,12 @@ bashprofile_path = File.join(home, ".bash_profile")
 profile_script_path = File.join(env_path, "profile.sh")
 
 execute "update .bashrc" do
-    command "echo \"source #{profile_script_path}\" >> #{bashrc_path}"
-    not_if "grep 'profile.sh' #{bashrc_path}"
+  command "echo \"source #{profile_script_path}\" >> #{bashrc_path}"
+  not_if "grep 'profile.sh' #{bashrc_path}"
 end
 
 execute "update .bash_profile" do
-    command "echo \"[[ -f \\\"\\\$HOME/.bashrc\\\" ]] && source \\\"\\\$HOME/.bashrc\\\"\" >> #{bashprofile_path}"
-    not_if "grep '.bashrc' #{bashprofile_path}"
+  command "echo \"[[ -f \\\"\\\$HOME/.bashrc\\\" ]] && source \\\"\\\$HOME/.bashrc\\\"\" >> #{bashprofile_path}"
+  not_if "grep '.bashrc' #{bashprofile_path}"
 end
 
--- chef/cookbooks/environment/recipes/ssh.rb
+++ chef/cookbooks/environment/recipes/ssh.rb
@@ -2,22 +2,22 @@ home = ENV['HOME']
 key_path = "#{home}/Google Drive/keys/touch@ungboriboonpisal.com.nopass"
 
 directory "#{home}/.ssh" do
-    recursive true
-    owner ENV["USER"]
+  recursive true
+  owner ENV["USER"]
 end
 
 template "#{home}/.ssh/config" do
-    source "ssh_config.erb"
-    mode "600"
-    owner ENV["USER"]
-    variables key_path: key_path
+  source "ssh_config.erb"
+  mode "600"
+  owner ENV["USER"]
+  variables key_path: key_path
 end
 
 file key_path do
-    mode "600"
+  mode "600"
 end
 
 execute "add key to keychain" do
-    command "ssh-add -K '#{key_path}'"
-    not_if "ssh-add -l | grep '#{key_path}'"
+  command "ssh-add -K '#{key_path}'"
+  not_if "ssh-add -l | grep '#{key_path}'"
 end
